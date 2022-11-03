ceb4c1098d98a887556dd7143f769b8a130a3fd9
lexS1989 <lexshadrin@mail.ru>
2022-11-03 13:05:24 +1100

    Третий коммит правка в PATTERN

--- src/main/java/pro/sky/telegrambot/service/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/service/TelegramBotUpdatesListener.java
@@ -28,7 +28,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private final NotificationTaskRepository notificationTaskRepository;
 
-    private final Pattern PATTERN = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
+    private final Pattern PATTERN = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+\0-9]+)");
 
     private final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
 
